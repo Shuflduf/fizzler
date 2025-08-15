fbe4a7829609c93100dd1cbee746e1b168fe19a0
Shuflduf <shuflduf@gmail.com>
2025-08-15 13:33:41 -0600

    underlike the liksn

--- src/routes/Resource.svelte
+++ src/routes/Resource.svelte
@@ -13,7 +13,10 @@
 		? 'flex-row-reverse'
 		: 'flex-row'}"
 >
-	<a href={link} class="group text-center font-[Arvo] text-2xl font-bold text-nowrap">
+	<a
+		href={link}
+		class="group text-center font-[Arvo] text-2xl font-bold text-nowrap underline decoration-2"
+	>
 		<img src={imgPath} alt="tauri" class="asp mx-auto mb-4 h-24 transition group-hover:scale-105" />
 		<p>{name}</p>
 	</a>
